aecf3971b9dec7b49202e018f64b293f62c96d57
Travis Patriarca <69721042+TravisPatriarca@users.noreply.github.com>
2022-03-06 14:49:57 +0800

    push

--- js/maze.js
+++ js/maze.js
@@ -6,11 +6,31 @@ class Maze {
         this.yCells         = Math.ceil(height / cellSize);
         this.cellSize       = cellSize;
         this.visitedCells   = [];
-        this.currentCell    = undefined;
-        this.cellCount      = xCells*yCells;
+        this.cellCount      = this.xCells*this.yCells;
+        this.fillNum        = 0;
+        this.cells          = this.iniCells(this.xCells, this.yCells, this.cellSize);
+        this.currentCell    = this.cells[0][0];
+        this.cellSets = []
+        // kruskal
+        this.edges = [];
+        this.cells.forEach((row, x) => {
+            row.forEach((column, y) => {
+                this.cells[x][y].walls.forEach((wall, i)  => {
+                    this.edges.push({
+                        x, 
+                        y,
+                        direction: i,
+                        cellSetId: x*y+y
+                    });
+                });
 
-        iniCells(this.xCells, this.yCells, this.cellSize)
+                this.cellSets[x*y+y] = [this.cells[x][y]];
+            });
+        });
+
+        //shuffle(this.edges);
     }
+
     iniCells(xCells, yCells, cellSize) {
         let cells = []
         for (let x = 0; x < xCells; x++) {
@@ -20,25 +40,83 @@ class Maze {
             }
             cells.push(row);
         }
-    
+
         return cells
     }
 
-    increment() {
+    getCellMeeting(cell, wallDirection) {
+        const x = cell.x;
+        const y = cell.y;
 
+        switch (wallDirection) {
+            case 0:
+                if (this.checkBounds(x, y-1, this.cells)) {
+                    return this.cells[x][y-1]
+                }
+            case 1:
+                if (this.checkBounds(x+1, y, this.cells)) {
+                    return this.cells[x+1][y]
+                }
+            case 2:
+                if (this.checkBounds(x, y+1, this.cells)) {
+                    return this.cells[x][y+1]
+                }
+            case 3:
+                if (this.checkBounds(x-1, y, this.cells)) {
+                    return this.cells[x-1][y]
+                }
+        }
+
+        return undefined;
+    }
+
+    incrementDFS() {
+        if (this.visitedCells.length > 0) {
+            let neighbours  = this.getNeighbors(this.currentCell.x, this.currentCell.y, this.cells);
+            neighbours      = neighbours.filter(neighbour => !neighbour.visited)
+            if (neighbours.length > 0) {
+                const randomNeighbour = neighbours[Math.floor(Math.random() * neighbours.length)];
+                this.currentCell.walls[this.currentCell.getWallMeeting(randomNeighbour)] = false;
+                randomNeighbour.walls[randomNeighbour.getWallMeeting(this.currentCell)] = false;
+                this.currentCell = randomNeighbour;
+                this.visitCell(this.currentCell, this.visitedCells);
+            }
+            else
+            {
+                this.currentCell = this.visitedCells.pop();
+            }
+        }
+    }
+
+    incrementKruskal() {
+        if (this.edges.length > 0) {
+            const edge = this.edges.pop();
+            const cell = this.cells[edge.x][edge.y]
+            const nCell = this.getCellMeeting(this.cells[edge.x][edge.y], edge.direction);
+
+            if (typeof nCell !== 'undefined') {
+                if (!this.cellSets[edge.cellSetId].includes(nCell)) {
+                    this.visitCell(cell);
+                    this.visitCell(nCell);
+
+                    cell.walls[cell.getWallMeeting(nCell)] = false;
+                    nCell.walls[nCell.getWallMeeting(cell)] = false;
+                }
+            }
+        }
     }
 
     visitCell(cell) {
-        cell.visited = true;
-        cell.filled = true;
-        cell.colour = "hsl(" + round(fillNum/cellCount*360) + ", " + round(random(80, 90)) + "%, 50%)";
+        cell.visited    = true;
+        cell.filled     = true;
+        cell.colour     = "hsl(" + round(this.fillNum/this.cellCount*360) + ", " + round(random(80, 90)) + "%, 50%)";
+
         cell.draw(true);
     
-        
-        fillNum++;
+        this.fillNum++;
         this.visitedCells.push(cell); 
     
-        let neighbours = getNeighbors2(cell.x, cell.y, cells);
+        let neighbours = this.getNeighbors(cell.x, cell.y, this.cells);
         neighbours.forEach((neighbour) => {
             neighbour.draw(true);
         }) 
@@ -50,36 +128,19 @@ class Maze {
         }
         return true;
     }
-
+    
     getNeighbors(x, y, cells) {
         let neighbors = [];
-        if (checkBounds(x - 1, y, cells) && !cells[x - 1][y].visited) {
+        if (this.checkBounds(x - 1, y, cells)) {
             neighbors.push(cells[x - 1][y]);
         }
-        if (checkBounds(x + 1, y, cells) && !cells[x + 1][y].visited) {
+        if (this.checkBounds(x + 1, y, cells)) {
             neighbors.push(cells[x + 1][y]);
         }
-        if (checkBounds(x, y - 1, cells) && !cells[x][y - 1].visited) {
+        if (this.checkBounds(x, y - 1, cells)) {
             neighbors.push(cells[x][y - 1]);
         }
-        if (checkBounds(x, y + 1, cells) && !cells[x][y + 1].visited) {
-            neighbors.push(cells[x][y + 1]);
-        }
-        return neighbors;
-    }
-    
-    getNeighbors2(x, y, cells) {
-        let neighbors = [];
-        if (checkBounds(x - 1, y, cells)) {
-            neighbors.push(cells[x - 1][y]);
-        }
-        if (checkBounds(x + 1, y, cells)) {
-            neighbors.push(cells[x + 1][y]);
-        }
-        if (checkBounds(x, y - 1, cells)) {
-            neighbors.push(cells[x][y - 1]);
-        }
-        if (checkBounds(x, y + 1, cells)) {
+        if (this.checkBounds(x, y + 1, cells)) {
             neighbors.push(cells[x][y + 1]);
         }
         return neighbors;
--- sketch.js
+++ sketch.js
@@ -1,119 +1,36 @@
-const width         = window.innerWidth;
-const height        = window.innerHeight;
-let cellSize      = 50;
-const xCells        = Math.ceil(width / cellSize);
-const yCells        = Math.ceil(height / cellSize);
-const cellCount     = xCells*yCells;
-let cells           = [];
-let visitedCells    = [];
-let currentCell     = undefined;
-let fillNum         = 0;
-let cx = 0, cy = 0;
-const iterations = 1;
+const width       = window.innerWidth;
+const height      = window.innerHeight;
+let cellSize      = 100;
+let iterations    = 1;
+const bgColour    = 30;
+let frameTime     = 200;
+let maze;
 
 function setup() {
-    createCanvas(width, height);
+    let canvas = createCanvas(width, height);
+    canvas.parent('container')
+    background(bgColour);
+    frameRate(frameTime);
 
-    cells = iniCells(xCells, yCells, cellSize);
-    currentCell = cells[0][0];
-    background(30);
-    frameRate(500);
-    visitCell(currentCell, visitedCells)
+    maze = new Maze(width, height, cellSize);
+    //maze.visitCell(maze.currentCell, maze.visitedCells)
+
+    $("#generate").click(() => {
+        clear();
+        resizeCanvas(windowWidth, windowHeight);
+        background(bgColour);
+        frameTime = parseInt($("#frameRate").val());
+        iterations = parseInt($("#iterations").val());
+        cellSize = parseInt($("#cellSize").val());
+        frameRate(frameTime);
+        maze = new Maze(width, height, cellSize);
+        maze.visitCell(maze.currentCell, maze.visitedCells) 
+    });
+
 }
 
 function draw() {
     for (let i=0; i<iterations; i++) {
-        if (visitedCells.length > 0) {
-            let neighbours = getNeighbors(currentCell.x, currentCell.y, cells);
-            if (neighbours.length > 0) {
-                const randomNeighbour = neighbours[Math.floor(Math.random() * neighbours.length)];
-                currentCell.walls[currentCell.getWallMeeting(randomNeighbour)] = false;
-                randomNeighbour.walls[randomNeighbour.getWallMeeting(currentCell)] = false;
-                currentCell = randomNeighbour;
-                visitCell(currentCell, visitedCells);
-            }
-            else
-            {
-                currentCell = visitedCells.pop();
-            }
-        }
+        maze.incrementKruskal();
     }
 }
-
-function visitCell(cell, visitedCells) {
-    cell.visited = true;
-    cell.filled = true;
-    cell.colour = "hsl(" + round(fillNum/cellCount*360) + ", " + round(random(80, 90)) + "%, 50%)";
-    cell.draw(true);
-
-    
-    fillNum++;
-    visitedCells.push(cell); 
-
-    let neighbours = getNeighbors2(cell.x, cell.y, cells);
-    neighbours.forEach((neighbour) => {
-        neighbour.draw(true);
-    }) 
-}
-
-function checkBounds(x, y, cells) {
-    if (x < 0 || x >= cells.length || y < 0 || y >= cells[0].length) {
-        return false;
-    }
-    return true;
-}
-
-function getNeighbors(x, y, cells) {
-    let neighbors = [];
-    if (checkBounds(x - 1, y, cells) && !cells[x - 1][y].visited) {
-        neighbors.push(cells[x - 1][y]);
-    }
-    if (checkBounds(x + 1, y, cells) && !cells[x + 1][y].visited) {
-        neighbors.push(cells[x + 1][y]);
-    }
-    if (checkBounds(x, y - 1, cells) && !cells[x][y - 1].visited) {
-        neighbors.push(cells[x][y - 1]);
-    }
-    if (checkBounds(x, y + 1, cells) && !cells[x][y + 1].visited) {
-        neighbors.push(cells[x][y + 1]);
-    }
-    return neighbors;
-}
-
-function getNeighbors2(x, y, cells) {
-    let neighbors = [];
-    if (checkBounds(x - 1, y, cells)) {
-        neighbors.push(cells[x - 1][y]);
-    }
-    if (checkBounds(x + 1, y, cells)) {
-        neighbors.push(cells[x + 1][y]);
-    }
-    if (checkBounds(x, y - 1, cells)) {
-        neighbors.push(cells[x][y - 1]);
-    }
-    if (checkBounds(x, y + 1, cells)) {
-        neighbors.push(cells[x][y + 1]);
-    }
-    return neighbors;
-}
-
-function iniCells(xCells, yCells, cellSize) {
-    let cells = []
-    for (let x = 0; x < xCells; x++) {
-        let row = [];
-        for (let y = 0; y < yCells; y++) {
-            row.push(new Cell(x, y, cellSize));
-        }
-        cells.push(row);
-    }
-
-    return cells
-}
-
-/*function drawCells(xCells, yCells, outline=false) {
-    for (let x = 0; x < xCells; x++) {
-        for (let y = 0; y < yCells; y++) {
-            cells[x][y].draw(outline);
-        }
-    }
-}*/
